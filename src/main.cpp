64af0985b77c14897bd4d0324ea64cf2d8286595
Thomas Goussaert <thomas.goussaert@gmail.com>
2024-11-15 12:16:26 +0100

    sfdsa

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,9 @@
 
 int main()
 {
-#if defined(WIN32) || defined(_WIN32)
-    aslkfjal
-#endif
+// #if defined(WIN32) || defined(_WIN32)
+//     aslkfjal
+// #endif
 
     std::cout << "Hello World!" << std::endl;
     return 0;
